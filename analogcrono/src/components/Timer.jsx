196fb55b94e0fedf37b769e4c39342865a8b65c3
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-01-11 06:49:37 +0000

    5 commit

--- analogcrono/src/components/Timer.jsx
+++ analogcrono/src/components/Timer.jsx
@@ -1,31 +1,41 @@
 import { useContext } from 'react';
 import { TimersContext } from '../context/analogcron';
 import '../App.css';
+import { useState } from 'react';
 
 const Timer = () => {
-
   const [TimersState, dispatch] = useContext(TimersContext);
   console.log(TimersState);
-  let [timer, setTime] = useState({
+
+  let [time, setTime] = useState({
     millis: '00',
     secs: '00',
     hours: '00',
   });
 
-  //the name changed and the idea too of timer
-  return <div className="container">
-    <div className="stopwatch">
-      <div className="heading-bar">
-        <span>Stopwatch</span>
-      <div className="settings-icon">⚙</div>
+  return (
+    <div className="container">
+      <div className="stopwatch">
+        <div className="heading-bar">
+          <span>Stopwatch</span>
+          <div className="settings-icon">⚙</div>
+        </div>
+        <div className="timer-container">
+          <div className="stripes">
+            <div className="timer">
+              {time.hours}:{time.secs}:{time.millis}
+            </div>
+          </div>
+        </div>
+        <div className="spacer">
+          <div className='button-container'>
+            <div className='button'>Start</div>
+            <div className='button'>Restart</div>            
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="timer-container">a
-    <div className="stripes">
-      <div className="timer"></div>
-    </div>
-    </div>
-  </div>
-  </div>
+  )
 };
 
 export default Timer;
