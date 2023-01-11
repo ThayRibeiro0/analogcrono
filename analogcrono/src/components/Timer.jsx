9338eef35f8f1cdd4bc926cb414a21b68c1291ff
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-01-11 03:38:55 +0000

    4 commit

--- analogcrono/src/components/Timer.jsx
+++ analogcrono/src/components/Timer.jsx
@@ -3,9 +3,14 @@ import { TimersContext } from '../context/analogcron';
 import '../App.css';
 
 const Timer = () => {
-  const [TimersState, dispatch] = useContext(TimersContext);
 
+  const [TimersState, dispatch] = useContext(TimersContext);
   console.log(TimersState);
+  let [timer, setTime] = useState({
+    millis: '00',
+    secs: '00',
+    hours: '00',
+  });
 
   //the name changed and the idea too of timer
   return <div className="container">
@@ -14,7 +19,11 @@ const Timer = () => {
         <span>Stopwatch</span>
       <div className="settings-icon">⚙</div>
     </div>
-    <div className="timer-container">a</div>
+    <div className="timer-container">a
+    <div className="stripes">
+      <div className="timer"></div>
+    </div>
+    </div>
   </div>
   </div>
 };
