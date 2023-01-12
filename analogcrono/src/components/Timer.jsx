ef058bd2be4ea62bcbcbfe3d2017d9bcd31d2e6a
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-01-12 06:19:53 +0000

    7 committt

--- analogcrono/src/components/Timer.jsx
+++ analogcrono/src/components/Timer.jsx
@@ -1,17 +1,87 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { TimersContext } from '../context/analogcron';
 import '../App.css';
 import { useState } from 'react';
 
+
+const defaultState = {
+  millis: '00',
+  secs: '00',
+  hours: '00',
+  minutes: '00',
+};
+
+let startDuration = null;
+
+
+function getZeroAppendedString(num){
+  let returnValue = ('0' + num).slice(-2);
+  return returnValue;
+}
+
+function calculateTime() {
+  let millisecondsPassed = new Date().getTime() - startDuration;
+  let millisecondsTimer =  String(millisecondsPassed % 1000).substring(0, 2);
+  let secondsPassed = Math.floor(millisecondsPassed / 1000);
+  let secondsPassedTimer = Math.floor(millisecondsPassed / 1000) % 60; 
+  let minutesPassed = Math.floor(secondsPassed / 60);
+  let minutesPassedTimer = Math.floor(secondsPassed / 60) % 60;
+  let hoursPassed = Math.floor(minutesPassed / 60) % 24;
+ 
+  secondsPassed = getZeroAppendedString(secondsPassedTimer);
+  minutesPassed = getZeroAppendedString(minutesPassedTimer);
+  hoursPassed = getZeroAppendedString(hoursPassed);
+  
+  return {
+    millis: millisecondsTimer,
+    secs: secondsPassed,
+    hours: hoursPassed,
+    minutes: minutesPassed,
+  };
+}
+
 const Timer = () => {
   const [TimersState, dispatch] = useContext(TimersContext);
   console.log(TimersState);
-
+  
   let [time, setTime] = useState({
-    millis: '01',
+    millis: '00',
     secs: '00',
     hours: '00',
+    minutes: '00',
   });
+  
+  let [isStarted, setIsStarted] = useState(false);
+  let [interval, _setInterval] = useState(null);
+  
+  useEffect(() => {
+    return () => {
+      if (interval){
+        clearInterval(interval);
+        _setInterval(null);
+      }
+    };
+  }, []);
+
+  function startTimer(){
+    if (!isStarted) {
+      startDuration = new Date().getTime();
+      _setInterval(
+        setInterval(() => {
+          setTime(calculateTime());
+        }, 20)
+      );   
+    }
+    setIsStarted(bIsStarted => !bIsStarted);
+  }
+
+  function resetTimer(){
+    setTime(defaultState);
+    setIsStarted(false);
+    clearInterval(interval);
+    _setInterval(null);
+    startDuration = null;
+  }
 
   return (
     <div className="container">
@@ -28,14 +98,18 @@ const Timer = () => {
         <div className="timer-container">
           <div className="stripes">
             <div className="timer">
-              {time.hours}:{time.secs}:{time.millis}
+              {time.hours}:{time.minutes}:{time.secs}:{time.millis}
             </div>
           </div>
         </div>
         <div className="spacer">
           <div className='button-container'>
-            <div className='button'>Start</div>
-            <div className='button'>Restart</div>            
+            <div onClick={startTimer} 
+            className={`button ${isStarted ? 'active' : ''}`}
+            >
+              {isStarted ? 'Stop' : 'Start'}
+            </div>
+            <div onClick={resetTimer} className='button'>Restart</div>            
           </div>
         </div>
       </div>
